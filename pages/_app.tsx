4adc0397bc40bdee98e6837d30daae96162f2343
mumtaz.eris <mumtaz.eris@softtech.com.tr>
2021-02-16 01:39:39 +0300

    Add material-ui integration and lightbulb icon to index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+import PropTypes from 'prop-types';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+  <Component {...pageProps} />
+  </ThemeProvider>
+  </React.Fragment>
+  );
 }
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { makeStyles } from '@material-ui/core/styles';
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
@@ -14,7 +17,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   }
 }
+function LightBulbIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+    </SvgIcon>
+  );
+}
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(6, 0, 3),
+  },
+  lightBulb: {
+    verticalAlign: 'middle',
+    marginRight: theme.spacing(1),
+  },
+}));
 export default function Home({
   allPostsData
 }: {
@@ -24,6 +43,7 @@ export default function Home({
     id: string
   }[]
 }) {
+  const classes = useStyles();
   return (
     <Layout home>
       <Head>
@@ -31,6 +51,7 @@ export default function Home({
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, this is <b>Cem</b>. I'm a software developer, and a ballroom dancer.</p>
+        <LightBulbIcon className={classes.lightBulb} />
         <p>
           This site is created using {' '}
           <a href="https://nextjs.org/learn"> Next.js tutorial</a>.
@@ -42,14 +63,14 @@ export default function Home({
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
